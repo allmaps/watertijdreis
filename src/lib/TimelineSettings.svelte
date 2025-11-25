8939c690094baa0da4490d3df4d4e0cb0131c946
josephinespit <josephine.spit@hotmail.com>
2025-11-25 17:23:57 +0100

    timeline settings component geüpdate, BIS als onderdeel van regulier

---
+++ src/lib/TimelineSettings.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { fade, scale } from 'svelte/transition';
+	import { Gear } from 'phosphor-svelte';
+
+	let { showSettings = false, toggleSettings } = $props();
+</script>
+
+<div class="pointer-events-auto absolute top-2 right-2 z-[50000] select-none">
+	<button
+		onclick={(e) => {
+			e.stopPropagation();
+			toggleSettings();
+		}}
+		title="Instellingen"
+		class="
+			flex h-8 w-8 cursor-pointer items-center justify-center
+			rounded-full bg-white text-[#444]
+			shadow-md transition-all
+			hover:scale-105 hover:shadow-lg
+            active:scale-95
+		"
+	>
+		<Gear size={18} />
+	</button>
+
+	{#if showSettings}
+		<div
+			id="settings-menu"
+			class="absolute w-80 rounded-lg border border-gray-200 bg-white px-3 py-3 shadow-lg transition-all duration-200"
+			style="right: 3.0rem; top: -130px;"
+			onclick={(e) => e.stopPropagation()}
+		>
+			<slot />
+		</div>
+	{/if}
+</div>
