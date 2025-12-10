14ebdb34cb79b13378ac792a0c3386956867624e
josephinespit <josephinespit98@gmail.com>
2025-12-10 12:31:32 +0100

    Undo gemeente layer test

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -594,16 +594,12 @@
 
 		await Promise.all(loadPromises);
 
-		// TODO: tijdelijk uitgezet
-		// const spriteJson = await fetch('/sprites/regular-sheets-128.json').then((resp) => resp.json());
-		// warpedMapLayer.addSprites(
-		// 	spriteJson,
-		// 	window.location.origin + '/sprites/regular-sheets-128.jpg',
-		// 	[3072, 3078]
-		// );
-
-		historicMapsLoaded = true;
-		applyFilter(filter);
+		const spriteJson = await fetch('/sprites/regular-sheets-128.json').then((resp) => resp.json());
+		warpedMapLayer.addSprites(
+			spriteJson,
+			window.location.origin + '/sprites/regular-sheets-128.jpg',
+			[3072, 3078]
+		);
 
 		map.addSource('map-outlines', {
 			type: 'geojson',
@@ -992,19 +988,25 @@
 		// 	tileSize: 256
 		// });
 
-		map.addLayer({
-			id: 'overlay-waterschapsgrenzen',
-			type: 'raster',
-			source: 'pdok-waterschapsgrenzen',
-			layout: { visibility: 'none' }
-		});
+		map.addLayer(
+			{
+				id: 'overlay-waterschapsgrenzen',
+				type: 'raster',
+				source: 'pdok-waterschapsgrenzen',
+				layout: { visibility: 'none' }
+			},
+			'warped-map-layer'
+		);
 
-		map.addLayer({
-			id: 'overlay-gemeentegrenzen',
-			type: 'raster',
-			source: 'pdok-gemeentegrenzen',
-			layout: { visibility: 'none' }
-		});
+		map.addLayer(
+			{
+				id: 'overlay-gemeentegrenzen',
+				type: 'raster',
+				source: 'pdok-gemeentegrenzen',
+				layout: { visibility: 'none' }
+			},
+			'warped-map-layer'
+		);
 
 		// map.addLayer(
 		// 	{
