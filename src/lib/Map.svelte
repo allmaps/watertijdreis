f4a469ce20526eaf638e65b3be120df9de078987
josephinespit <josephinespit98@gmail.com>
2025-12-10 15:42:52 +0100

    remove sheetcontrols

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -14,7 +14,7 @@
 	import Timeline2 from './Timeline2.svelte';
 	import MapSheetToggle from './MapSheetToggle.svelte';
 	import MapButtons from './MapButtons.svelte';
-	import SheetControls from './SheetControls.svelte';
+	// import SheetControls from './SheetControls.svelte';
 	import { getUserLocation } from '$lib/UserLocation.svelte';
 	import { basemapStyle } from './basemap';
 
@@ -1618,7 +1618,7 @@
 	{setGridVisibility}
 ></Timeline2>
 
-<SheetControls {visibleHistoricMaps} {selectedHistoricMap} {changeHistoricMapView}></SheetControls>
+<!-- <SheetControls {visibleHistoricMaps} {selectedHistoricMap} {changeHistoricMapView}></SheetControls> -->
 
 <Minimap
 	{visibleHistoricMaps}
