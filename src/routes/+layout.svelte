6b754b1b21c8abcbf79fb81eb7e09c1d43ce54dd
Jules Schoonman <jules.schoonman@gmail.com>
2025-12-13 13:51:36 +0100

    fix for umami tracker & use dynamic env vars

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import '../app.css';
+	import { UmamiAnalytics, trackPageView } from '@lukulent/svelte-umami';
+	import { env } from '$env/dynamic/public';
 
 	let { children } = $props();
+
+	const websiteID = env.PUBLIC_UMAMI_WEBSITE_ID;
+	const srcURL = env.PUBLIC_UMAMI_SRC;
 
 	let i = 0;
 
 	onMount(() => {
+		trackPageView();
 		const icons = ['waves.png', 'waves_flipped.png'];
 
 		setInterval(() => {
@@ -20,3 +26,13 @@
 </script>
 
 {@render children()}
+
+{#if websiteID && srcURL}
+	<UmamiAnalytics
+		{websiteID}
+		{srcURL}
+		configuration={{
+			'data-auto-track': false
+		}}
+	/>
+{/if}
